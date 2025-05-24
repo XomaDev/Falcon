bdaaa1c1ceaefd2134eb34d9f89a44add695c00e
kumaraswamy <care.xoma@gmail.com>
2025-05-24 15:41:17 +0530

    pretty file names

---
+++ src/ast/control/if.go
@@ -0,0 +1,61 @@
+package control
+
+import (
+	"Falcon/ast/blockly"
+	"strings"
+)
+
+type If struct {
+	Conditions []blockly.Expr
+	Bodies     [][]blockly.Expr
+	ElseBody   []blockly.Expr
+}
+
+func (i *If) String() string {
+	var builder strings.Builder
+
+	numConditions := len(i.Conditions)
+	currI := 0
+
+	builder.WriteString("if ")
+	for {
+		builder.WriteString(i.Conditions[currI].String())
+		builder.WriteString(" {\n")
+		builder.WriteString(blockly.PadBody(i.Bodies[currI]))
+		builder.WriteString("}")
+		currI++
+		if currI < numConditions {
+			builder.WriteString(" elif ")
+		} else {
+			break
+		}
+	}
+	if i.ElseBody != nil {
+		builder.WriteString(" else {\n")
+		builder.WriteString(blockly.PadBody(i.ElseBody))
+		builder.WriteString("}")
+	}
+	return builder.String()
+}
+
+func (i *If) Blockly() blockly.Block {
+	conditions := blockly.ValuesByPrefix("IF", i.Conditions)
+	bodies := blockly.ToStatements("DO", i.Bodies)
+
+	numbElifs := len(i.Conditions) - 1
+	var numbElse int
+
+	if i.ElseBody != nil {
+		bodies = append(bodies, blockly.CreateStatement("ELSE", i.ElseBody))
+		numbElse = 1
+	} else {
+		numbElifs = 0
+	}
+	return blockly.Block{
+		Type:       "controls_if",
+		Mutation:   &blockly.Mutation{ElseIfCount: numbElifs, ElseCount: numbElse},
+		Values:     conditions,
+		Statements: bodies,
+		Consumable: false,
+	}
+}
--- src/ast/control/if_expr.go
+++ src/ast/control/if_expr.go
@@ -2,60 +2,23 @@ package control
 
 import (
 	"Falcon/ast/blockly"
-	"strings"
+	"Falcon/sugar"
 )
 
-type If struct {
-	Conditions []blockly.Expr
-	Bodies     [][]blockly.Expr
-	ElseBody   []blockly.Expr
+type SimpleIf struct {
+	Condition blockly.Expr
+	Then      blockly.Expr
+	Else      blockly.Expr
 }
 
-func (i *If) String() string {
-	var builder strings.Builder
-
-	numConditions := len(i.Conditions)
-	currI := 0
-
-	builder.WriteString("if ")
-	for {
-		builder.WriteString(i.Conditions[currI].String())
-		builder.WriteString(" {\n")
-		builder.WriteString(blockly.PadBody(i.Bodies[currI]))
-		builder.WriteString("}")
-		currI++
-		if currI < numConditions {
-			builder.WriteString(" elif ")
-		} else {
-			break
-		}
-	}
-	if i.ElseBody != nil {
-		builder.WriteString(" else {\n")
-		builder.WriteString(blockly.PadBody(i.ElseBody))
-		builder.WriteString("}")
-	}
-	return builder.String()
+func (s *SimpleIf) String() string {
+	return sugar.Format("if (%) % else %", s.Condition.String(), s.Then.String(), s.Else.String())
 }
 
-func (i *If) Blockly() blockly.Block {
-	conditions := blockly.ValuesByPrefix("IF", i.Conditions)
-	bodies := blockly.ToStatements("DO", i.Bodies)
-
-	numbElifs := len(i.Conditions) - 1
-	var numbElse int
-
-	if i.ElseBody != nil {
-		bodies = append(bodies, blockly.CreateStatement("ELSE", i.ElseBody))
-		numbElse = 1
-	} else {
-		numbElifs = 0
-	}
+func (s *SimpleIf) Blockly() blockly.Block {
 	return blockly.Block{
-		Type:       "controls_if",
-		Mutation:   &blockly.Mutation{ElseIfCount: numbElifs, ElseCount: numbElse},
-		Values:     conditions,
-		Statements: bodies,
-		Consumable: false,
+		Type:       "controls_choose",
+		Values:     blockly.MakeValues([]blockly.Expr{s.Condition, s.Then, s.Else}, "TEST", "THENRETURN", "ELSERETURN"),
+		Consumable: true,
 	}
 }
