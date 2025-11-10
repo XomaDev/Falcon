a2dcad21cb6465c49bd433a75831ddd2416f1602
Kumaraswamy B G <care.xoma@gmail.com>
2025-11-10 17:24:47 +0530

    fix if-else blocly_mutation wrong metadata

--- language/code/ast/control/if.go
+++ language/code/ast/control/if.go
@@ -1,14 +1,14 @@
 package control
 
 import (
-	ast2 "Falcon/code/ast"
+	"Falcon/code/ast"
 	"strings"
 )
 
 type If struct {
-	Conditions []ast2.Expr
-	Bodies     [][]ast2.Expr
-	ElseBody   []ast2.Expr
+	Conditions []ast.Expr
+	Bodies     [][]ast.Expr
+	ElseBody   []ast.Expr
 }
 
 func (i *If) Yail() string {
@@ -20,12 +20,12 @@ func (i *If) Yail() string {
 		yail += "(if "
 		yail += cond.Yail()
 		yail += "(begin "
-		yail += ast2.PadBodyYail(i.Bodies[k])
+		yail += ast.PadBodyYail(i.Bodies[k])
 		yail += ")"
 	}
 	if i.ElseBody != nil {
 		yail += " (begin "
-		yail += ast2.PadBodyYail(i.ElseBody)
+		yail += ast.PadBodyYail(i.ElseBody)
 		yail += ")"
 	}
 	for k := 0; k < len(i.Conditions)-1; k++ {
@@ -45,7 +45,7 @@ func (i *If) String() string {
 	for {
 		builder.WriteString(i.Conditions[currI].String())
 		builder.WriteString(" {\n")
-		builder.WriteString(ast2.PadBody(i.Bodies[currI]))
+		builder.WriteString(ast.PadBody(i.Bodies[currI]))
 		builder.WriteString("}")
 		currI++
 		if currI < numConditions {
@@ -56,28 +56,28 @@ func (i *If) String() string {
 	}
 	if i.ElseBody != nil {
 		builder.WriteString(" else {\n")
-		builder.WriteString(ast2.PadBody(i.ElseBody))
+		builder.WriteString(ast.PadBody(i.ElseBody))
 		builder.WriteString("}")
 	}
 	return builder.String()
 }
 
-func (i *If) Blockly() ast2.Block {
-	conditions := ast2.ValuesByPrefix("IF", i.Conditions)
-	bodies := ast2.ToStatements("DO", i.Bodies)
+func (i *If) Blockly() ast.Block {
+	conditions := ast.ValuesByPrefix("IF", i.Conditions)
+	bodies := ast.ToStatements("DO", i.Bodies)
 
-	numbElifs := len(i.Conditions) - 1
+	numbElifs := len(conditions) - 1
 	var numbElse int
 
 	if i.ElseBody != nil {
-		bodies = append(bodies, ast2.CreateStatement("ELSE", i.ElseBody))
+		bodies = append(bodies, ast.CreateStatement("ELSE", i.ElseBody))
 		numbElse = 1
 	} else {
-		numbElifs = 0
+		numbElse = 0
 	}
-	return ast2.Block{
+	return ast.Block{
 		Type:       "controls_if",
-		Mutation:   &ast2.Mutation{ElseIfCount: numbElifs, ElseCount: numbElse},
+		Mutation:   &ast.Mutation{ElseIfCount: numbElifs, ElseCount: numbElse},
 		Values:     conditions,
 		Statements: bodies,
 	}
